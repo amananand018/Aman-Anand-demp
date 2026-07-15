705277ecb9e1bf33d2d6ad17a97a2eb1e5e96a8b
amananand018 <anandaman212@gmail.com>
2026-07-16 00:39:21 +0530

    Add calculator function with basic operations
    Implement a basic calculator function that supports addition, subtraction, multiplication, and division with input validation.

--- Calci.py
+++ Calci.py
@@ -1,1 +1,37 @@
+def calculator():
+    
+    valid_operators = ["+","-","*","/"]
+
+    while True:
+        
+        num1 = float(input("Enter your number"))
+        operators = input("Enter your operators(+,-,*,/):")
+        if operators not in valid_operators:
+            print("Invalid operators")
+            continue
+        num2=float(input("Enter your 2nd number"))
+        
+        if operators == "+":
+            result = num1 + num2
+        elif operators == "-":
+            result = num1 - num2
+        elif operators == "*":
+            result = num1 * num2
+        elif operators == "/":
+
+
+            if num2 == 0:
+                print("Invalid as can't divide with zero")
+                continue
+            else:
+                result == num1 / num2
+
+        print(f"\nResult: {num1} {operators} {num2} = {result}\n")
+
+
+calculator()
+
+
+                    
+            
 
